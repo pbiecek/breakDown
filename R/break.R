18511c601575a80f3985579a7b114b4b66582804
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-11-18 16:02:26 +0100

    + support for glms

--- R/break.R
+++ R/break.R
@@ -43,6 +43,41 @@ broken.lm <- function(model, new_observation, ...) {
   create.broken(broken_intercept)
 }
 
+#' Create the broken object for glm models
+#'
+#' @param model a glm model
+#' @param new_observation a new observation with collumns that corresponds to variables used in the model
+#' @param ... other parameters
+#'
+#' @return an object of the broken class
+#' @export
+#' @importFrom stats predict.lm
+#'
+#' @examples
+#' url <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv'
+#' wine <- read.table(url, header = T, sep=";")
+#' wine$qualityb <- factor(wine$quality > 5.5, labels = c("bad", "good"))
+#' modelg <- glm(qualityb~fixed.acidity + volatile.acidity + citric.acid + residual.sugar + chlorides + free.sulfur.dioxide + total.sulfur.dioxide + density + pH + sulphates + alcohol,
+#'     data=wine, family = "binomial")
+#' new_observation <- wine[1,]
+#' br <- broken(modelg, new_observation)
+#' plot(br)
+
+broken.glm <- function(model, new_observation, ...) {
+  ny <- predict.glm(model, newdata = new_observation, type="terms")
+
+  broken_obj <- data.frame(variable = paste(colnames(ny),  "=",
+                                            sapply(new_observation[colnames(ny)], as.character)),
+                           contribution = c(ny))
+  broken_sorted <- broken_obj[order(-abs(broken_obj$contribution)),]
+  broken_intercept <- rbind(
+    data.frame(variable = "(Intercept)",
+               contribution = attributes(ny)$constant),
+    broken_sorted)
+
+  create.broken(broken_intercept)
+}
+
 broken.xgboost <- function() {
 
 }
