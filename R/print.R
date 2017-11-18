252f9c7715cb6621e81f05362581bffabdc5eafb
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-11-18 16:02:14 +0100

    + print for broken

---
+++ R/print.R
@@ -0,0 +1,15 @@
+#' Break Down Print
+#'
+#' @param x the model model of 'broken' class
+#' @param ... other parameters
+#'
+#' @return a data frame
+#'
+#' @examples
+#' @export
+print.broken <- function(x, ...) {
+  broken_cumm <- x
+  class(broken_cumm) = "data.frame"
+  broken_cumm$contribution <- signif(broken_cumm$contribution, 2)
+  broken_cumm[,c("variable", "contribution")]
+}
