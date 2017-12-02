1358936aefa2999c6f580301bb6b8b2eda7bdb4c
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2017-12-02 21:24:51 +0100

    fix - partial solution for #3

--- R/break.R
+++ R/break.R
@@ -99,7 +99,67 @@ broken.glm <- function(model, new_observation, ..., baseline = 0) {
   create.broken(broken_sorted, baseline)
 }
 
-broken.xgboost <- function() {
 
+#' Create the broken object for ranger models
+#'
+#' @param model a ranger model
+#' @param new_observation a new observation with collumns that corresponds to variables used in the model
+#' @param ... other parameters
+#' @param baseline the orgin/baseline for the breakDown plots, where the rectangles start. It may be a number or a character "Intercept". In the latter case the orgin will be set to model intercept.
+#'
+#' @return an object of the broken class
+#'
+#' @examples
+#' library(ranger)
+#' model <- ranger(factor(left) ~ ., data = HR_data, importance = 'impurity')
+#' importance(model)
+#' new_observation <- HR_data[89,]
+#' # here
+#' # put
+#' # examples
+#'
+#' @export
+
+broken.ranger <- function(model, new_observation, ..., baseline = 0) {
+
+  yhat <- ranger:::predict.ranger(model, new_observation, predict.all=TRUE)
+  terminalNodes <- ranger:::predict.ranger(model, new_observation, type="terminalNodes")$predictions
+  varNames <- model$forest$independent.variable.names
+  # find variables on the path
+  selectedVarsForAllTrees <- lapply(seq_along(terminalNodes), function(i){
+    nodesLeft <- model$forest$child.nodeIDs[[i]][[1]]
+    nodesRight <- model$forest$child.nodeIDs[[i]][[2]]
+    nonZeroLeft <- which(nodesLeft > 0)
+    nonZeroRight <- which(nodesRight > 0)
+    parents <- numeric(length(nodesLeft))
+    parents[nodesLeft[nonZeroLeft] + 1] <- nonZeroLeft
+    parents[nodesRight[nonZeroRight] + 1] <- nonZeroRight
+
+    startSearch <- terminalNodes[i] + 1
+    allVars <- model$forest$split.varIDs[[i]]
+    selectedVars <- numeric(length(varNames))
+    while (parents[startSearch] > 0) {
+      selectedVars[allVars[parents[startSearch]]] <- 1
+      startSearch <- parents[startSearch]
+    }
+    selectedVars/sum(selectedVars)
+  })
+
+  varImportance <- matrix(0, nrow = 2, ncol = length(varNames))
+  for (j in seq_along(yhat$predictions[1,])) {
+    wclass <- yhat$predictions[1,j]
+    varImportance[wclass,] <- varImportance[wclass,] + selectedVarsForAllTrees[[j]]
+  }
+  colnames(varImportance) <- varNames
+
+
+  broken_obj <- data.frame(variable = paste(varNames,  "=",
+                                            sapply(new_observation[varNames], as.character)),
+                           contribution = (varImportance[1,] - varImportance[2,])/(2*sum(varImportance)))
+  broken_sorted <- broken_obj[order(-abs(broken_obj$contribution)),]
+
+  baseline <- 0.5
+
+  create.broken(broken_sorted, baseline)
 }
 
