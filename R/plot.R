188aa37a74f87d081e99f7764c8debd912bd373a
Aleksandra Grudziąż <oll.dabrowska@gmail.com>
2018-06-05 22:54:15 +0200

    update plot function

--- R/plot.R
+++ R/plot.R
@@ -21,45 +21,48 @@
 #' model <- lm(quality~., data=wine)
 #' new_observation <- wine[1,]
 #' br <- broken(model, new_observation)
+#' plot(br)
+#' plot(br, top_features = 2)
 #' plot(br, top_features = 2, min_delta = 0.01)
 #'
 #' @export
-plot.broken <- function(x, trans = I, ..., top_features=0, min_delta=0, add_contributions = TRUE,
+plot.broken <- function(x, trans = I, ..., top_features = 0, min_delta = 0, add_contributions = TRUE,
                         vcolors = c("-1" = "#d8b365", "0" = "#f5f5f5", "1" = "#5ab4ac", "X" = "darkgrey"),
                         digits = 3, rounding_function = round, plot_distributions = FALSE) {
   position <- cummulative <- prev <- trans_contribution <- prediction <- label <- id <- NULL
   
-  if(top_features>0){
-    if(top_features>=(length(x[["variable"]])-1)){
+  if(top_features > 0){
+    if(top_features >= (length(x[["variable"]])-1)){
       message(paste0("We have only ", length(x[["variable"]])-1, " variables in our model."))
     }else{
-    contributions <- x[["contribution"]][1:(length(x[["contribution"]])-1)]
+    contributions <- x[["contribution"]]
+    contributions <- contributions[-length(contributions)]
     contributions <- abs(contributions)
     top_values <- contributions[order(contributions, decreasing = T)][1:top_features]
     logical_condition <- abs(x[["contribution"]]) %in% top_values
-    logical_condition[length(logical_condition)]<-TRUE #we always want to have the final prognosis
+    logical_condition[length(logical_condition)] <- TRUE #we always want to have the final prognosis
     }
   }
   
-  if(min_delta>0){
-    if((min_delta>max(abs(x[["contribution"]])))){
+  if(min_delta > 0){
+    if((min_delta > max(abs(x[["contribution"]])))){
       message(paste0("The maximum stroke of variables in our model is ", max(abs(x[["contribution"]]))))
     }else{
-    logical_condition_2 <- abs(x[["contribution"]])>min_delta
-    logical_condition_2[length(logical_condition_2)]<-TRUE #we always want to have the final prognosis
+    logical_condition_2 <- abs(x[["contribution"]]) > min_delta
+    logical_condition_2[length(logical_condition_2)] <- TRUE #we always want to have the final prognosis
     
     }
   }
   
-  if((top_features>0 & top_features<(length(x[["variable"]])-1)) & (min_delta>0 & min_delta<=max(abs(x[["contribution"]])))){
+  if((top_features > 0 & top_features < (length(x[["variable"]])-1)) & (min_delta > 0 & min_delta <= max(abs(x[["contribution"]])))){
     logical_condition <- "&"(logical_condition, logical_condition_2)
-  }else if(top_features>0 & top_features<(length(x[["variable"]])-1)){
+  }else if(top_features > 0 & top_features < (length(x[["variable"]])-1)){
     logical_condition <- logical_condition
-  }else if((min_delta>0 & min_delta<=max(abs(x[["contribution"]])))){
-    logical_condition<-logical_condition_2
+  }else if((min_delta > 0 & min_delta <= max(abs(x[["contribution"]])))){
+    logical_condition <- logical_condition_2
   }
     
-  if((top_features>0 & top_features<(length(x[["variable"]])-1))|| (min_delta>0 & min_delta<=max(abs(x[["contribution"]])))){
+  if((top_features > 0 & top_features < (length(x[["variable"]])-1))|| (min_delta > 0 & min_delta <= max(abs(x[["contribution"]])))){
     list_names <- names(x)
     df <- list()
     for(i in 1:length(x)){
@@ -79,16 +82,17 @@ plot.broken <- function(x, trans = I, ..., top_features=0, min_delta=0, add_cont
     position_rest <- length(df[["variable"]])
     sign_rest <- sign(contribution_rest)
     cummulative_rest <- df[["cummulative"]][(length(df[["cummulative"]])-1)] + contribution_rest
-    constant<-attr(x, "baseline")
+    constant <- attr(x, "baseline")
     attr(df, "baseline") <- constant
-    distribution<-attr(x, "yhats_distribution")
+    distribution <- attr(x, "yhats_distribution")
     attr(df, "yhats_distribution") <- distribution
     
     x<-df
     
-    for(i in 1:(length(x[["position"]]))){
+    for(i in seq_along(x[["position"]])){
       x[["position"]][i] <- i
     }
+    
     x[["position"]][length(x[["position"]])] <- (length(x[["position"]])+1)
     
     
@@ -98,7 +102,7 @@ plot.broken <- function(x, trans = I, ..., top_features=0, min_delta=0, add_cont
     x[["sign"]][length(x[["variable"]])] <- sign_rest
     x[["cummulative"]][length(x[["variable"]])] <- cummulative_rest  
     x[["position"]][length(x[["variable"]])] <- position_rest
-    x[["variable_name"]][length(x[["variable"]])]<-""
+    x[["variable_name"]][length(x[["variable"]])] <- ""
     x[["variable_value"]][length(x[["variable"]])] <- ""
   }
 
@@ -118,7 +122,7 @@ plot.broken <- function(x, trans = I, ..., top_features=0, min_delta=0, add_cont
     constant <- attr(broken_cumm, "baseline")
     broken_cumm$prev <- trans(constant + broken_cumm$cummulative - broken_cumm$contribution)
     broken_cumm$cummulative <- trans(constant + broken_cumm$cummulative)
-    if((min_delta>0 & min_delta<=max(abs(x[["contribution"]]))) || (top_features>0 & top_features<(length(x[["variable"]])-1))){
+    if((min_delta > 0 & min_delta <= max(abs(x[["contribution"]]))) || (top_features > 0 & top_features < (length(x[["variable"]])-1))){
       broken_cumm <- as.data.frame(broken_cumm)
       broken_cumm <- broken_cumm[c(1:(nrow(broken_cumm)-2), nrow(broken_cumm), (nrow(broken_cumm)-1)),]
     }else{
